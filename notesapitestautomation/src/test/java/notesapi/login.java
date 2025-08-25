eb4a4c808cf9f4b32eaa3a5807773c901344b8d0
pradnya1399 <59558579+pradnya1399@users.noreply.github.com>
2025-08-26 00:46:32 +0530

    saved token value in a variable in login class and use it in the profile case directly.

--- notesapitestautomation/src/test/java/notesapi/login.java
+++ notesapitestautomation/src/test/java/notesapi/login.java
@@ -6,15 +6,23 @@ import io.restassured.path.json.JsonPath;
 
 public class login {
 
-	public static void main(String[] args) {
+	public  String loginuser()
+	{
 		String Response=RestAssured.given().contentType("application/x-www-form-urlencoded; charset=UTF-8")
-		.formParams("email", "prad123@gmail.com","password","abc@123")
-		.when().post("https://practice.expandtesting.com/notes/api/users/login")
-		.then().statusCode(200).assertThat().body("message",equalTo("Login successful")).extract().asPrettyString();
-		System.out.println(Response);
-		JsonPath js= new JsonPath(Response);
-		String token= js.getString("data.token");
-		System.out.println(token);
+				.formParams("email", "prad123@gmail.com","password","abc@123")
+				.when().post("https://practice.expandtesting.com/notes/api/users/login")
+				.then().statusCode(200).assertThat().body("message",equalTo("Login successful")).extract().asPrettyString();
+				System.out.println(Response);
+				JsonPath js= new JsonPath(Response);
+				String token= js.getString("data.token");
+				System.out.println(token);
+		return token;
+	}
+
+	public static void main(String[] args) {
+		login l1= new login();
+		l1.loginuser();
+		
 	}
 
 }
--- notesapitestautomation/src/test/java/notesapi/profile.java
+++ notesapitestautomation/src/test/java/notesapi/profile.java
@@ -6,10 +6,12 @@ import static org.hamcrest.Matchers.equalTo;
 public class profile {
 
 	public static void main(String[] args) {
-		RestAssured.given().header("x-auth-token","2b743b32f0c04288aa18cf1d522a5099d085963f94d2438bab887b1a69313c82")
+		login l= new login();
+		String newtoken= l.loginuser();
+		RestAssured.given().header("x-auth-token",newtoken)
 		.header("accept","application/json")
 		.when().get("https://practice.expandtesting.com/notes/api/users/profile")
-		.then().statusCode(200).assertThat().body("message", equalTo("Profile successful"));
+		.then().log().all().statusCode(200).assertThat().body("message", equalTo("Profile successful"));
 	}
 
 }
